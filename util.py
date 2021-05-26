cbd29cc30b0fc45595d85da9c53111bbb3cc3fcd
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-05-26 17:47:41 +0100

    Add `create_folder` function

--- util.py
+++ util.py
@@ -31,3 +31,19 @@ def file_search(dirname, sub_files, skip_dir=[]):
 
         else:
             sub_files.append(full_filename)
+
+
+def create_folder(dirname):
+    '''
+    Creates a folder with the given file path.
+
+        Parameters:
+            dirname (string): File path to be created i.e, full path name
+
+        Returns:
+            None
+    '''
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+    else:
+        print(f"File path --> '{dirname}' already exists")
